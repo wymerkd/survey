b3741ca9de77d922e69f33c7e64c6eda6aeead0a
wymerkd <wymerkd@gmail.com>
2019-02-23 13:01:48 -0800

    add values to form questions and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-var pizza = $("input:radio[name=pizza]:checked").val();
+$(function () {
+  $("form#questionnaire").submit(function(event) {
+    event.preventDefault();
+    $("#question1").hide();
+    $("#question2").hide();
+    $("#question3").hide();
+    $("#question4").hide();
+    $("#question5").hide();
+    var total = parseInt($("input:radio[name=pizza]:checked").val());
+      total += parseInt($("input:radio[name=personality]:checked").val());
+      total += parseInt($("input:radio[name=time]:checked").val());
+      total += parseInt($("input:radio[name=location]:checked").val());
+      total += parseInt($("input:radio[name=drinks]:checked").val());
+
+      console.log(total);
+
+      if (total >= 30) {
+        $("#michaelangelo").show();
+      } else if (total >= 24 && total <30) {
+        $("#leonardo").show();
+      } else if (total >=14 && total <20) {
+        $("#donatello").show();
+      } else {
+        $("#rafael").show();
+      }
+  ;})
+
+;})
